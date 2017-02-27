855a31ecdce4f3120d56d88e64461a56688633df
ColonolNutty <ColonolNutty@hotmail.com>
2017-02-26 18:28:55 -0700

    Fix the analog clock

--- objects/wired/data/analogclock/analogclock.lua
+++ objects/wired/data/analogclock/analogclock.lua
@@ -7,6 +7,7 @@ end
 function update(dt)
   local timeOfDay = world.timeOfDay()
   local theta = storage.zeroAngle - (math.pi * 2 * timeOfDay)
+  animator.resetTransformationGroup("hand")
   animator.rotateTransformationGroup("hand", theta)
   object.setOutputNodeLevel(0, (timeOfDay <= 0.5))
 end
